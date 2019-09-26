bb3acbef33d08df69376cec6a16a2f665f49f3d3
igorsambo <igorlsambo1999@icloud.com>
2019-09-27 00:31:46 +0200

    Added a few more documents
    - added a profile /page
    - added a new /fonts
    - added web support

--- lib/pages/perfil.dart
+++ lib/pages/perfil.dart
@@ -3,8 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import '../widgets/LogOutButon.dart';
 
 class Perfil extends StatefulWidget {
-
   FirebaseUser currentUser;
+
   //Perfil(currentUser);
 
   @override
@@ -15,21 +15,55 @@ class Perfil extends StatefulWidget {
 }
 
 class _Perfil extends State {
-
   FirebaseUser _currentUser;
 
   //_Perfil(_currentUser);
   @override
   Widget build(BuildContext context) {
+     Map<String,String>grupos = new Map();
+     grupos['grupo1'] = 'drena group';
+     grupos['grupo2'] = 'family gathering';
+     grupos['grupo3'] = 'gift and party';
+     grupos['grupo4'] = 'lets take shots and party, and sure some gifts';
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text('Perfil')
-        ,actions: <Widget>[LogOutButton()],
+        title: Text('Perfil'),
+        actions: <Widget>[LogOutButton()],
       ),
-      body: Center(
-        child: ClipOval(child:Text('ok', style: TextStyle(fontSize: 70.0),)),
-        ),
-      );
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 80.0,
+                  child: Image.asset('icon/icon.png'),
+                ),
+                Text(
+                  'nickname',
+                  style: TextStyle(
+                      fontFamily: 'KhalidPersonal', color: Colors.black),
+                )
+              ],
+            ),
+            heightFactor: 10.0,
+            widthFactor: 40.0,
+          ),
+          ListView.builder(
+            itemCount: grupos.length,
+            itemBuilder: (context, index) => ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+              ),
+              title: Text(grupos.keys.toString()),
+              subtitle: Text(grupos.values.toString()),
+
+            ),
+          )
+        ],
+      ),
+      backgroundColor: Colors.blueGrey,
+    );
   }
 }
